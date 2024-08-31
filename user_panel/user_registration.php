59420ab3dbcfc5a37017ac8baf454a04e96f4d78
STARRED787 <malithamalshan7@gmail.com>
2024-09-01 00:34:26 +0530

    PHP

--- user_panel/user_registration.php
+++ user_panel/user_registration.php
@@ -14,10 +14,40 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Akaya+Kanadaka&family=Anta&family=Barrio&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=RocknRoll+One&display=swap"
+        rel="stylesheet">
 </head>
+<style>
+    .box {
+        width: 930px;
+    }
 
-<body>
+    .bg-i {
+        background-color: rgb(11, 47, 159);
+    }
 
+    .font {
+        font-family: "Anta";
+    }
+</style>
+
+<body class="font">
+    <div class="container d-flex justify-content-center align-items-center min-vh-100 ">
+        <div class="row rounder-5 p-3 bg-white shadow box ">
+            <div class="col-md-6 left-box rounded d-flex flex-column bg-i justify-content-center align-items-center">
+                <dic class="fetured-img">
+                    <img src="./images/registration.jpg" class="img-fliud mt-2" width="430px">
+                </dic>
+                <p class=" text-white fs-2 text-center">Registraion</p>
+                <small class=" text-white text-wrap text-center">SUN GSM Community</small>
+            </div>
+            <div class="col-md-6 right-box"></div>
+
+        </div>
+    </div>
 </body>
 
 </html>

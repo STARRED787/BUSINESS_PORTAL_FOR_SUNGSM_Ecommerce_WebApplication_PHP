0f8f6fc64ba718cbb0fd998277c570a3d025e773
STARRED787 <malithamalshan7@gmail.com>
2024-09-22 10:50:43 +0530

    PHP

--- admin_panel/index.php
+++ admin_panel/index.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Admin Dashboard</title>
   <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
-  <link rel="stylesheet" href="../admin_panel/index.css" />
+  <link rel="stylesheet" href="./index.css" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
@@ -16,61 +16,52 @@
 
 <body>
   <!--Admin Bar-->
-  <div class="container-fluid ">
-    <div class="row flex-nowrap">
-      <div class="adminbar col-auto col-md-3 col-xl-3 px-sm-2 px-0 bg-dark">
+  <div class="container-fluid">
+    <div class="row">
+      <!-- Sidebar -->
+      <div class="col-auto col-md-3 col-xl-3 px-sm-2 px-0 bg-dark">
         <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100 font">
           <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
             <span class="fs-5 d-none d-sm-inline">Admin Dashboard</span>
           </a>
-
           <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
             <li class="nav-item">
               <a href="index.php?insert_product" class="nav-link align-middle px-0 text-white">
-                <i class="bx bxs-home" style="font-weight: 900"></i>
+                <i class="bx bxs-home"></i>
                 <span class="ms-1 d-none d-sm-inline">Insert Products</span>
               </a>
-
-              <a href="./index.php?view_product.php" class="nav-link align-middle px-0 text-white">
+              <a href="index.php?view_product" class="nav-link align-middle px-0 text-white">
                 <i class="bx bxs-package"></i>
                 <span class="ms-1 d-none d-sm-inline">View Products</span>
               </a>
-
               <a href="index.php?insert_categori" class="nav-link align-middle px-0 text-white">
                 <i class="fa-solid fa-layer-group"></i>
                 <span class="ms-1 d-none d-sm-inline">Insert Categories</span>
               </a>
-
               <a href="#" class="nav-link align-middle px-0 text-white">
                 <i class="fa-solid fa-list"></i>
                 <span class="ms-1 d-none d-sm-inline">View Categories</span>
               </a>
-
               <a href="index.php?insert_brand" class="nav-link align-middle px-0 text-white">
                 <i class="fa-solid fa-tag"></i>
                 <span class="ms-1 d-none d-sm-inline">Insert Brands</span>
               </a>
-
               <a href="#" class="nav-link align-middle px-0 text-white">
                 <i class="fa-solid fa-tags"></i>
                 <span class="ms-1 d-none d-sm-inline">View Brands</span>
               </a>
-
               <a href="#" class="nav-link align-middle px-0 text-white">
                 <i class="fa-solid fa-truck"></i>
-                <span class="ms-1 d-none d-sm-inline">All Oders</span>
+                <span class="ms-1 d-none d-sm-inline">All Orders</span>
               </a>
-
               <a href="#" class="nav-link align-middle px-0 text-white">
                 <i class="fa-solid fa-credit-card"></i>
-                <span class="ms-1 d-none d-sm-inline">All Payements</span>
+                <span class="ms-1 d-none d-sm-inline">All Payments</span>
               </a>
-
               <a href="#" class="nav-link align-middle px-0 text-white">
                 <i class="fa-solid fa-user"></i>
                 <span class="ms-1 d-none d-sm-inline">List Users</span>
               </a>
-
               <a href="#" class="nav-link align-middle px-0 text-white">
                 <i class="fa-solid fa-right-from-bracket"></i>
                 <span class="ms-1 d-none d-sm-inline">Logout</span>
@@ -80,7 +71,9 @@
           <hr />
         </div>
       </div>
-      <div>
+
+      <!-- Content Area -->
+      <div class="col py-3">
         <?php
         if (isset($_GET['insert_categori'])) {
           include('insert_categories.php');
@@ -90,6 +83,9 @@
         }
         if (isset($_GET['insert_product'])) {
           include('insert_product.php');
+        }
+        if (isset($_GET['view_product'])) {
+          include('view_product.php');
         }
         ?>
       </div>
---
+++ admin_panel/view_product.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css"
+        referrerpolicy="no-referrer" />
+    <title>Edit Account</title>
+</head>
+
+<body>
+    <!---- PHP code to get User Id to access orders table----->
+
+    <div style="border-radius: 15px; font-family:Poppins">
+        <div class="card">
+            <div class="card-body">
+                <h1 class="text-center ">My Orders</h1>
+
+                <h1>view</h1>
+
+            </div>
+        </div>
+    </div>
+
+
+
+    <!-- Corrected: Removed duplicate Bootstrap JS inclusion -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+
+    <!-- jQuery (required for Toastr) -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <!-- Toastr JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"
+        referrerpolicy="no-referrer"></script>
+</body>
+
+</html>

542d0e7df54b4c73e3c800bcd1c50ce6a697c63c
STARRED787 <malithamalshan7@gmail.com>
2024-11-06 15:28:25 +0530

    zHP

--- admin_panel/index.php
+++ admin_panel/index.php
@@ -39,7 +39,7 @@ if (isset($_SESSION['admin_username'])) {
   background-repeat: no-repeat; 
   background-size: cover;
   background-attachment: fixed;
-"">
+">
     <script src=" https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" referrerpolicy="no-referrer">
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"

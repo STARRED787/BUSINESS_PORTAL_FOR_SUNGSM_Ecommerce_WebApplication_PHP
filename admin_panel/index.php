64238c8d3a8208858a9c36a685802949e836a143
STARRED787 <malithamalshan7@gmail.com>
2024-11-06 15:27:51 +0530

    PHP

--- admin_panel/index.php
+++ admin_panel/index.php
@@ -31,9 +31,17 @@ if (isset($_SESSION['admin_username'])) {
         referrerpolicy="no-referrer" />
 </head>
 
-<body style="background-color: #f8f9fa;">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
-        referrerpolicy="no-referrer"></script>
+<body style="
+  background-image: url(./images/back.jpg);
+  height: 580px; 
+  width: 100%;
+  background-position: center;
+  background-repeat: no-repeat; 
+  background-size: cover;
+  background-attachment: fixed;
+"">
+    <script src=" https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" referrerpolicy="no-referrer">
+    </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"
         referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
--- owner_panel/index.php
+++ owner_panel/index.php
@@ -31,7 +31,15 @@ if (isset($_SESSION['owner_username'])) {
         referrerpolicy="no-referrer" />
 </head>
 
-<body style="background-color: #f8f9fa;">
+<body style="
+  background-image: url(./image/back.jpg);
+  height: 580px; 
+  width: 100%;
+  background-position: center;
+  background-repeat: no-repeat; 
+  background-size: cover;
+  background-attachment: fixed;
+">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
         referrerpolicy="no-referrer"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"

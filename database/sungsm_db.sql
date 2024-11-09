aa1fe8dc54b321d898a0a1e1d64cf3044863afca
STARRED787 <malithamalshan7@gmail.com>
2024-11-09 15:57:15 +0530

    PHP

--- database/sungsm_db.sql
+++ database/sungsm_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 07, 2024 at 10:15 PM
+-- Generation Time: Nov 09, 2024 at 11:26 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -174,7 +174,8 @@ INSERT INTO `delivery_details` (`delivery_id`, `user_id`, `order_id`, `delivery_
 (3, 6, 3, '193', '0717796266', 'Delivered', '2024-11-09', 'Standard', 'Prompt Express', '2024-11-02 15:41:51', 1, '1212wq11111'),
 (4, 6, 4, '193', '0717796266', 'Delivered', '2024-11-09', 'Standard', 'Prompt Express', '2024-11-02 15:48:05', 1, 'Delivered'),
 (5, 6, 5, '193', '0717796266', 'Delivered', '2024-11-10', 'Standard', 'Prompt Express', '2024-11-02 16:04:20', 1, 'werrrttt'),
-(6, 6, 6, '193', '0717796266', 'Pending', NULL, 'Standard', 'Prompt Express', '2024-11-07 18:48:15', 0, NULL);
+(6, 6, 6, '193', '0717796266', 'Shipped', '2024-11-10', 'Standard', 'Prompt Express', '2024-11-07 18:48:15', 1, '1212wq11567'),
+(7, 6, 9, '193', '0717796266', 'Pending', NULL, 'Standard', 'Prompt Express', '2024-11-09 09:35:22', 0, NULL);
 
 -- --------------------------------------------------------
 
@@ -204,7 +205,10 @@ INSERT INTO `orders` (`order_id`, `user_id`, `amount_due`, `invoice_number`, `to
 (3, 6, 60000, 1806626306, 1, '2024-11-02 15:42:02', 'Complete', 1, 0),
 (4, 6, 60000, 146470748, 1, '2024-11-02 15:49:53', 'Complete', 1, 0),
 (5, 6, 155000, 1733804316, 1, '2024-11-02 16:04:31', 'Complete', 1, 0),
-(6, 6, 155000, 15582117, 1, '2024-11-07 18:40:18', 'Pending', 0, 0);
+(6, 6, 155000, 15582117, 1, '2024-11-07 18:40:18', 'Pending', 0, 0),
+(7, 6, 0, 533241104, 0, '2024-11-07 22:33:19', 'Cancelled', 0, 0),
+(8, 6, 0, 1598760462, 0, '2024-11-07 22:35:37', 'Cancelled', 0, 0),
+(9, 6, 300, 1479816708, 1, '2024-11-09 09:35:52', 'Complete', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -231,7 +235,10 @@ INSERT INTO `orders_pending` (`order_id`, `user_id`, `invoice_number`, `product_
 (3, 6, 1806626306, 12, 1, 'Pending'),
 (4, 6, 146470748, 12, 1, 'Pending'),
 (5, 6, 1733804316, 8, 1, 'Pending'),
-(6, 6, 15582117, 8, 1, 'Pending');
+(6, 6, 15582117, 8, 1, 'Pending'),
+(7, 6, 533241104, 8, 0, 'Pending'),
+(8, 6, 1598760462, 12, 0, 'Pending'),
+(9, 6, 1479816708, 2, 1, 'Pending');
 
 -- --------------------------------------------------------
 
@@ -279,7 +286,6 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`product_id`, `product_tittle`, `product_description`, `product_keyword`, `categorie_id`, `brand_id`, `product_image1`, `product_image2`, `product_image3`, `product_price`, `date`, `status`) VALUES
-(2, 'Call of Duty 4- Modern Warfare', 'Call of Duty 4: Modern Warfare is a 2007 first-person shooter video game developed by Infinity Ward ', 'COD Call of Duty 4- Modern Warfare Call of Duty 4 DVD', 16, 7, 'game1.jpg', 'game1_2.jpg', 'game1_3.jpg', 300, '2024-11-02 09:19:56', 'true'),
 (3, 'God of War Ragnarok', 'God of War Ragnarök is a 2022 action-adventure game developed by Santa Monica Studio and published by Sony Interactive Entertainment. It is the ninth installment in the God of War series, the ninth chronologically, and the sequel to 2018\'s God of War. Set', 'God of War Ragnarok', 16, 7, 'god-of-war-ragnarok-2_1.jpeg', 'god-of-war-2018-featured-image.jpg', 'god-of-war-pc-screenshot-03-en-12oct21.jfif', 700, '2024-11-02 08:32:24', 'true'),
 (6, 'Resident Evil 7: Biohazard', 'Resident Evil 7: Biohazard is a 2017 survival horror game developed and published by Capcom. The player controls Ethan Winters as he searches for his long-missing wife in a derelict plantation occupied by an infected family, solving puzzles and fighting e', 'Resident Evil 7: Biohazard', 16, 7, 'cxd9vkFOAHVwwYG7lQKENGkrfyoAChNh.avif', 'RESIDENT-EVIL-7-biohazard_3840├u2160.jpg', 'topic2.jpg', 700, '2024-11-02 09:00:42', 'true'),
 (7, 'Dell 3511 MX350 ', 'Core i5 Processor, 8GB Ram, 512GB SSD NVMe, Nvidia Geforce MX350 Graphics, 15.6″ inch FHD Display, Office Home Silver, Windows 10', 'Dell 3511 MX350 ', 14, 15, 'Untitled-2.jpg', '7.jpg', '6.jpg', 175000, '2024-11-02 09:19:31', 'true'),
@@ -290,7 +296,8 @@ INSERT INTO `products` (`product_id`, `product_tittle`, `product_description`, `
 (12, 'Apple iPhone X', 'iPhone X - 64GB ROM + 3GB RAM - 12MP Rear/7MP Front - 5.8\" - 2716 mAh - Various Colors Color Options: Silver, Space Gray Display: 5.8-inch Super Retina display', 'Apple iPhone X ', 7, 17, 'images (2).jfif', 'Apple-iPhone-X.webp', 'iphone-x-silver.jpg', 60000, '2024-11-02 09:50:59', 'true'),
 (13, 'SPEAKER BT JBL BOOM MINI G02', 'Connectivity: Bluetooth Power Output: 5W Battery Life: Up to 10 hours Charging Time: Approximately 2.5 hours Water Resistance: IPX7 (waterproof) Dimensions: 3.74 x 3.74 x 5.51 inches (95 x 95 x 140 mm) Weight: 1.09 pounds (495 grams)', 'SPEAKER BT JBL BOOM MINI G02', 15, 18, 'images (4).jfif', 'O051130.jpg', 'images (3).jfif', 5000, '2024-11-02 10:21:19', 'true'),
 (14, 'Samsung MJ-6699', 'Samsung MJ-6699 - Microwave Oven Type: Solo Microwave Oven Capacity: 23 Liters Power Levels: 6 power levels Microwave Power: 800 Watts Control Type: Mechanical knobs Turntable: Yes, for even cooking Dimensions: 18.5 x 12.2 x 14.5 inches (W x H x D) Weight', 'Samsung MJ-6699', 15, 11, '16826149391280378990-samsung-mj-6699-level-on-pro-bluetooth-wireless-noise-cancelling-headphones.jpg', '1682614939241707569-samsung-mj-6699-level-on-pro-bluetooth-wireless-noise-cancelling-headphones.jpg', '16826149391134037749-samsung-mj-6699-level-on-pro-bluetooth-wireless-noise-cancelling-headphones.jpg', 3000, '2024-11-02 10:25:17', 'true'),
-(15, 'FANTECH X9 THOR', 'Type: Wired Gaming Mouse Sensor: Optical sensor with high-precision tracking DPI: Adjustable DPI up to 4800 for accurate responsiveness Buttons: 6 programmable buttons for custom controls Lighting: RGB LED backlighting with multiple color options Polling ', 'FANTECH X9 THOR', 15, 7, '0c296c6e2a3caf93df247e32dc624f8d.jpg', '459-20231128032738-X16v2_black_icon_758x455.png', '4-11.png', 2800, '2024-11-02 10:31:57', 'true');
+(15, 'FANTECH X9 THOR', 'Type: Wired Gaming Mouse Sensor: Optical sensor with high-precision tracking DPI: Adjustable DPI up to 4800 for accurate responsiveness Buttons: 6 programmable buttons for custom controls Lighting: RGB LED backlighting with multiple color options Polling ', 'FANTECH X9 THOR', 15, 7, '0c296c6e2a3caf93df247e32dc624f8d.jpg', '459-20231128032738-X16v2_black_icon_758x455.png', '4-11.png', 2800, '2024-11-02 10:31:57', 'true'),
+(16, 'Call of Duty 4 Modern Warfare', 'Call of Duty 4: Modern Warfare is a 2007 first-person shooter video game developed by Infinity Ward and published by Activision. It is the fourth main installment in the Call of Duty series. The game breaks away from the World War II setting of previous e', 'COD Call of Duty 4: Modern Warfare Call of Duty 4', 16, 7, 'game1.jpg', 'game1_2.jpg', 'game1_3.jpg', 300, '2024-11-09 10:04:14', 'true');
 
 -- --------------------------------------------------------
 
@@ -317,7 +324,8 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`user_id`, `username`, `user_email`, `password`, `user_image`, `user_ip`, `user_address`, `user_mobile`, `registration_at`) VALUES
 (6, 'malitha', 'malithamalshan7@gmail.com', '$2y$10$57vig2z39dg31PP2sh24e.XSYXts17SkoFCOoPK/IxKxZCkjZNUYq', 'rgistration.jpg', '::1', '193', '0717796266', '2024-11-02 15:32:36'),
 (8, 'irshad', 'mbmirshad@gmail.com', '$2y$10$u30eKAm4vYF0JsyuIQ5vTe/weJlMQHiOf5LVkKqg9rEzQ7HW6Ax3.', 'about.png', '::1', '217C Hanifa Road ', '0777489030', '2024-10-30 07:08:36'),
-(9, 'Pamoda', 'vijayanayakap97@gmail.com', '$2y$10$.QSqn9gJhdMEKlj6NwQrWuTZHAmlhIT/VIRtJsdnbul2fsAJouTVq', 'game1.jpg', '::1', '123/2', '0777489030', '2024-10-30 07:43:05');
+(9, 'Pamoda', 'vijayanayakap97@gmail.com', '$2y$10$.QSqn9gJhdMEKlj6NwQrWuTZHAmlhIT/VIRtJsdnbul2fsAJouTVq', 'game1.jpg', '::1', '123/2', '0777489030', '2024-10-30 07:43:05'),
+(10, 'buddika', 'buddi5@gmail.com', '$2y$10$YrdufyyvTBDnRwMjK5Rb6exrUZ3D39Xu07cMOB0NRRjXpksMI.Et.', 'සුහද දියවර  20231211_233830.jpg', '::1', '123/2', '0777489030', '2024-11-09 06:50:55');
 
 -- --------------------------------------------------------
 
@@ -343,7 +351,8 @@ INSERT INTO `user_payements` (`payement_id`, `order_id`, `invoice_number`, `amou
 (2, 2, 900199838, 300, 'Cash On Delivery', '2024-11-02 15:41:06'),
 (3, 3, 1806626306, 60000, 'Cash On Delivery', '2024-11-02 15:41:57'),
 (4, 4, 146470748, 60000, 'Cash On Delivery', '2024-11-02 15:49:48'),
-(5, 5, 1733804316, 155000, 'Cash On Delivery', '2024-11-02 16:04:25');
+(5, 5, 1733804316, 155000, 'Cash On Delivery', '2024-11-02 16:04:25'),
+(6, 9, 1479816708, 300, 'Cash On Delivery', '2024-11-09 09:35:41');
 
 --
 -- Indexes for dumped tables
@@ -466,19 +475,19 @@ ALTER TABLE `customer_feedback`
 -- AUTO_INCREMENT for table `delivery_details`
 --
 ALTER TABLE `delivery_details`
-  MODIFY `delivery_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `delivery_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `orders_pending`
 --
 ALTER TABLE `orders_pending`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `owners`
@@ -490,19 +499,19 @@ ALTER TABLE `owners`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `product_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `user_payements`
 --
 ALTER TABLE `user_payements`
-  MODIFY `payement_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `payement_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
